97771b30342f96a425fb1e0334a1226cc39ecbbd
Aleš <mit.mit@seznam.cz>
2023-02-23 16:16:40 +0100

    click Menu

--- js/script.js
+++ js/script.js
@@ -4,35 +4,16 @@ const secondaryMenu = document.querySelector(".headerMenu_change");
 const headerScroll = document.querySelector(".headerScroll")
 const body = document.querySelector("body")
 
-// menu.addEventListener("click", function(event){
-//     event.preventDefault();
-//     if(mainMenu === mainMenu){
-//         secondaryMenu.style.display = "block"
-//         headerScroll.style.display = "block"
-//         mainMenu.style.display = "none"
-//   }else{
-//       secondaryMenu.style.display = "none"
-//       headerScroll.style.display = "none"
-//       mainMenu.style.display = "block"
-//     }
-//   })
-  
-  
-  body.addEventListener("click", function(event){
-    console.log("da");
-    event.preventDefault();
-    event.stopPropagation()
-    if(mainMenu.style.display === "block"){
-      mainMenu.style.display = "none";
-      secondaryMenu.style.display = "block";
-      secondaryMenu.style.top = "-53px";
-      secondaryMenu.style.right = "0"
-    } else {
-      mainMenu.style.display = "block";
-      secondaryMenu.style.display = "none";
-    }
-  });
-  
+  mainMenu.addEventListener('click', function(){
+    secondaryMenu.style.display = "block"
+    secondaryMenu.style.top = "-53px";
+    secondaryMenu.style.right = "0"
+    mainMenu.style.display = "none"
+  })
+  secondaryMenu.addEventListener('click', function(){
+    secondaryMenu.style.display = "none";
+    mainMenu.style.display = "block"
+  })
   body.addEventListener("click", function(event){
     if(!event.target.closest(".headerRight") && !event.target.closest(".scrollMenu")){
       mainMenu.style.display = "block";
